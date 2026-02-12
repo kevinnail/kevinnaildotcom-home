2b2793b8daaa24155d967f2366c785470245be72
kevinnail <fumalicious@gmail.com>
2026-02-11 21:43:42 -0800

    Update ProjectList component to apply grid layout for improved styling

--- src/components/projects/ProjectList.jsx
+++ src/components/projects/ProjectList.jsx
@@ -3,7 +3,7 @@ import ProjectCard from './ProjectCard';
 
 export default function ProjectList() {
   return (
-    <div>
+    <div className="grid gap-6 px-2 py-4 md:px-4 lg:grid-cols-2">
       {projects.map((project) => (
         <ProjectCard key={project.title} project={project} />
       ))}
