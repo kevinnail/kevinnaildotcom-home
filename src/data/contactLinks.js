e3694101256c15f59a570ff1eb669e9b2062b21d
kevinnail <fumalicious@gmail.com>
2026-02-11 22:15:11 -0800

    Enhance contactLinks data structure with ariaLabel for improved accessibility

--- src/data/contactLinks.js
+++ src/data/contactLinks.js
@@ -1,8 +1,24 @@
 const contactLinks = [
-  { label: 'My Github', href: 'https://www.github.com/kevinnail' },
-  { label: 'Email', href: 'mailto:kevin@kevinnail.com' },
-  { label: 'LinkedIn', href: 'https://www.linkedin.com/in/kevinnail' },
-  { label: 'Instagram', href: 'https://www.instagram.com/stresslessglass' },
+  {
+    label: 'GitHub',
+    href: 'https://www.github.com/kevinnail',
+    ariaLabel: 'Kevin Nail on GitHub',
+  },
+  {
+    label: 'Email',
+    href: 'mailto:kevin@kevinnail.com',
+    ariaLabel: 'Email Kevin Nail',
+  },
+  {
+    label: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/kevinnail',
+    ariaLabel: 'Kevin Nail on LinkedIn',
+  },
+  {
+    label: 'Instagram',
+    href: 'https://www.instagram.com/stresslessglass',
+    ariaLabel: 'Stressless Glass on Instagram',
+  },
 ];
 
 export default contactLinks;
