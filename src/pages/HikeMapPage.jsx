3d5c420c2a35b540f3ed01368661e65d56c863dc
kevinnail <fumalicious@gmail.com>
2026-07-25 00:33:29 -0700

    pass compact to Banner on backpacking page

--- src/pages/HikeMapPage.jsx
+++ src/pages/HikeMapPage.jsx
@@ -74,7 +74,7 @@ export default function HikeMapPage() {
       </Helmet>
 
       <div className="flex h-dvh w-screen flex-col overflow-hidden bg-black">
-        <Banner />
+        <Banner compact />
         <div className="flex flex-1 flex-col-reverse overflow-hidden md:flex-row">
           <MapSidebar
             trips={trips}
