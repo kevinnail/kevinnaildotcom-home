f24495b29f7676572b287205a3489b62682f7b89
kevinnail <fumalicious@gmail.com>
2026-08-01 18:43:24 -0700

    update cors

--- server/lib/app.js
+++ server/lib/app.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: '1mb' }));
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
+    origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://100.118.149.112:5173'],
     credentials: true,
   }),
 );
