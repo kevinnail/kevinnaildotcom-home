6e87b2321acba47d5c760d52bd4a2b4259288ca9
kevinnail <fumalicious@gmail.com>
2026-08-04 01:13:51 -0700

    add FRONTEND_URL_2 to cors

--- server/lib/app.js
+++ server/lib/app.js
@@ -17,7 +17,13 @@ app.use(express.json({ limit: '1mb' }));
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://100.118.149.112:5173'],
+    origin: [
+      process.env.FRONTEND_URL,
+      process.env.FRONTEND_URL_2,
+      ,
+      'http://localhost:5173',
+      'http://100.118.149.112:5173',
+    ],
     credentials: true,
   }),
 );
